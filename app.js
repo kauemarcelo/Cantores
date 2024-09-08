22991cb7b68283039358a37fdf120b48d659374f
kauemarcelo <86077642+kauemarcelo@users.noreply.github.com>
2024-09-08 09:55:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+   
+
+    let campoPesquisa = document.getElementById 
+    ("campo-pesquisa").value 
+
+    if(!campoPesquisa) {
+        section.innerHTML = "Nada foi encontrado. Você precisa digitar o nome do cantor"
+        return 
+    }
+
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    console.log(campoPesquisa)
+    
+    let resultado =  "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    
+    for (let dado of dados) {
+        titulo = dado.titulo.toLocaleLowerCase()
+        descricao = dado.descricao.toLocaleLowerCase()
+         tags  = dado.tags.toLocaleLowerCase()
+        if (titulo.includes(campoPesquisa) ||
+         descricao.includes(campoPesquisa) || 
+            tags.includes(campoPesquisa)) 
+         {
+         resultado += `
+        <div class="item-resultado"> 
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo} </a>
+                </h2> 
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href= ${dado.link} target="_blank">Mais informações</a>
+                    </div>
+    `
+          ;
+    
+        }
+       
+    }
+    
+    if (!resultado) {
+        resultado = "<p> Nada foi encontrado</p>"
+    }
+   
+        
+    
+     section.innerHTML = resultado
+}
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Gusttavo Lima",
+        descricao: "Gusttavo Lima, nome artístico de Nivaldo Batista Lima, é um dos maiores nomes da música sertaneja brasileira. Nascido em Presidente Olegário, Minas Gerais, em 1989, o cantor iniciou sua carreira musical ainda muito jovem, tocando em banda  com seus irmãos e em dupas sertanejas",
+        link: "https://pt.wikipedia.org/wiki/Gusttavo_Lima", 
+        tags: "Embaixador" 
+    },
+    {
+        titulo: "Gustavo Mioto",
+        descricao: "Cantor sertanejo brasileiro conhecido por suas músicas românticas e voz marcante. Alcançou grande sucesso com hits como Balanço e Anti-Amor",
+        link: 'https://pt.wikipedia.org/wiki/Gustavo_Mioto',
+        tags : "anti-amor"
+        
+    },
+    {
+        titulo: "Ana Castela",
+        descricao: "Cantora sertaneja brasileira, conhecida como a Boiadeira. Se destacou no cenário musicalpor sua voz potente e letras que retratam a vida no campo. É uma das maiores revelações do sertanejo nos últimos anos.",
+        link: "https://pt.wikipedia.org/wiki/Ana_Castela",
+        tags: "boiadeira"
+    }
+];
